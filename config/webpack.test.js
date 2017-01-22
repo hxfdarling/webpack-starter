fed8570029a15fb29734d9d148e678e059a35d61
zman <hxfdarling@hotmail.com>
2017-01-22 16:37:24 +0800

    fix test

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -1,19 +1,10 @@
 var commonConfig = require('./webpack.common.js')('test');
 commonConfig.module.rules.push({
 	test: /\.spec\.js$/,
-	include: /src/,
+	include: /test/,
 	enforce: "pre",
 	exclude: /(bower_components|node_modules)/,
 	loader: 'babel-loader',
-	options: {
-		cacheDirectory: true,
-	}
-}, {
-	test: /\.js?$/,
-	include: /src/,
-	enforce: "pre",
-	exclude: /(node_modules|bower_components|\.spec\.js)/,
-	loader: 'babel-istanbul-loader',
 	options: {
 		cacheDirectory: true,
 	}
